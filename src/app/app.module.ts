cc8f7b44eb2c4778ac618f62b11277f38b4af545
MaxThom <maxthomassin@hotmail.com>
2017-08-17 18:21:43 -0400

    Added ShoppingList and Auth Modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,39 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AppRoutingModule } from './app-routing.module';
 import { RecipeService } from 'app/recipes/recipe-entities/recipe.service';
 import { HttpModule } from '@angular/http';
 import { RecipeStorageService } from './recipes/recipe-entities/recipe-storage.service';
-import { SignupComponent } from './auth/signup/signup.component';
-import { SigninComponent } from './auth/signin/signin.component';
 import { AuthService } from './auth/auth.service';
 import { AuthGuardService } from './auth/auth-guard.service';
 import { RecipeModule } from './recipes/recipe.module';
 import { SharedModule } from './shared/shared.module';
+import { ShoppingListModule } from './shopping-list/shopping-list.module';
+import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ShoppingListComponent,
-    ShoppingEditComponent,
-    SignupComponent,
-    SigninComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
     AppRoutingModule,
     HttpModule,
-    // TODO: To be deleted
-    ReactiveFormsModule,
+    FormsModule,
     RecipeModule,
+    ShoppingListModule,
+    AuthModule,
     SharedModule
   ],
   providers: [ShoppingListService, RecipeService, RecipeStorageService, AuthService, AuthGuardService],
---
+++ src/app/shopping-list/shopping-list.module.ts
@@ -0,0 +1,17 @@
+import { ShoppingEditComponent } from './shopping-edit/shopping-edit.component';
+import { ShoppingListComponent } from './shopping-list.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    ReactiveFormsModule
+  ],
+  declarations: [
+    ShoppingEditComponent,
+    ShoppingListComponent
+  ]
+})
+export class ShoppingListModule { }
